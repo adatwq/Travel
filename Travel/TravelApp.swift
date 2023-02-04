1f03f435531abe2d5ca244df4f6b09dd85968c30
Arwakh2 <78403707+Arwakh2@users.noreply.github.com>
2023-02-04 19:07:24 +0300

    Initial Commit

---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by Arwa on 13/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
